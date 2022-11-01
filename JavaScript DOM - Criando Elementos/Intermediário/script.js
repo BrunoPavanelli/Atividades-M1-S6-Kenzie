9c16be2fa61628be99ac59c83f3d7e3d67690d75
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-11-01 18:08:57 -0300

    JS - DOM: Intermediario finalizado

---
+++ JavaScript DOM - Criando Elementos/Intermediário/script.js
@@ -0,0 +1,6 @@
+const body = document.querySelector('body')
+const tagInput = document.createElement('input')
+tagInput.placeholder = 'Digite seu texto aqui'
+const form = document.querySelector('form')
+body.appendChild(form)
+form.appendChild(tagInput)
